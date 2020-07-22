9e04c9caf3388a2bf6102f078e3a747beaae8dbe
HoangTrieuThang <valentine.tnhn@gmail.com>
2020-07-22 14:25:19 +0700

    Security JPA

---
+++ spring-security-jpa/src/test/java/com/thang/springsecurityjpa/SpringSecurityJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thang.springsecurityjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
