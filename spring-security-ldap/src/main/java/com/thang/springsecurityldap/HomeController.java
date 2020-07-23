3b6a26b314f55df6d4bbba5bb71de4c54e730349
HoangTrieuThang <valentine.tnhn@gmail.com>
2020-07-23 14:06:50 +0700

    Add security-ldap

---
+++ spring-security-ldap/src/main/java/com/thang/springsecurityldap/HomeController.java
@@ -0,0 +1,14 @@
+package com.thang.springsecurityldap;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "Welcome to home page";
+    }
+}
